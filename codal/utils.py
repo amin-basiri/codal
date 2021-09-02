b710fcebed8d6731a4f2f90cc9c1bad9badff135
mehdi <mehdibs313@gmail.com>
2021-09-03 01:42:17 +0430

    Add Model Instance Serializer And Deserializer

---
+++ codal/utils.py
@@ -0,0 +1,15 @@
+from django.utils.encoding import force_text
+from django.apps import apps
+
+
+def serialize_instance(instance):
+    """ Serialize Django model instance """
+    model_name = force_text(instance._meta)
+    return '{}:{}'.format(model_name, instance.pk)
+
+
+def deserialize_instance(serialized_instance):
+    """ Deserialize Django model instance """
+    model_name, pk = serialized_instance.split(':')
+    model = apps.get_model(model_name)
+    return model._default_manager.get(pk=pk)
