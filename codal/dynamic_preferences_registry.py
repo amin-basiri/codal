f7131bea785331fa26aed4bae775e2b6b86c6ed1
mehdi <mehdibs313@gmail.com>
2021-09-03 22:24:40 +0430

    Edit update_from_datetime Preference

--- codal/dynamic_preferences_registry.py
+++ codal/dynamic_preferences_registry.py
@@ -1,8 +1,10 @@
-from dynamic_preferences.types import DateTimePreference
+from dynamic_preferences.types import StringPreference
 from dynamic_preferences.registries import global_preferences_registry
 
 
-class UpdateFromDatetime(DateTimePreference):
+@global_preferences_registry.register
+class UpdateFromDatetime(StringPreference):
     name = 'update_from_datetime'
+    default = ""
     required = False
 
