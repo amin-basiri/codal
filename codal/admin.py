7d517a11001fa2333342dd53c3e3c3117909e9fe
mehdi <mehdibs313@gmail.com>
2021-09-09 12:13:59 +0430

    Edit Letter Admin

--- codal/admin.py
+++ codal/admin.py
@@ -3,7 +3,7 @@ from codal.models import Letter
 
 
 class LetterAdmin(admin.ModelAdmin):
-    list_display = ['tracing_no', 'symbol', 'status']
+    list_display = ['title', 'tracing_no', 'symbol', 'status']
 
 
 admin.site.register(Letter, LetterAdmin)
