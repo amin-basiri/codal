f2574e18e62ee2d41b885f8187b04183502a738c
mehdi <mehdibs313@gmail.com>
2021-09-16 01:23:47 +0430

    Test And Debug Update Task [5]

--- codal/handlers.py
+++ codal/handlers.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 def set_end_to_task(sender, instance, created=False, **kwargs):
-    if instance.tracker.previous('status') == Task.Statuses.RUNNING and\
+    if instance.tracker.previous('status') == Task.Statuses.RUNNING and instance.tracker.has_changed('status') and \
             instance.status in [Task.Statuses.DONE, Task.Statuses.ERRED]:
         instance.end = timezone.now()
         instance.save(update_fields=['end'])
