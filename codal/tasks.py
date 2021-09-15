6e087bd2a23f0c49c18d3db81f4f4f05c1de18cd
mehdi <mehdibs313@gmail.com>
2021-09-16 00:09:50 +0430

    Edit download_retrieved Task

--- codal/tasks.py
+++ codal/tasks.py
@@ -74,69 +74,69 @@ def update():
 def download_retrieved_letter():
     Log.objects.create(
         type=Log.Types.INFO,
-        message="دانلود گزارشات دانلود نشده شروع شد.",
+        message="دانلود گزارشات دانلود نشده شروع شد",
         error=""
     )
-    #
-    # un_downloaded_letters = Letter.objects.filter(status=Letter.Statuses.RETRIEVED)
-    #
-    # global_preferences = global_preferences_registry.manager()
-    # download_pdf_pref = global_preferences['download_pdf']
-    # download_content_pref = global_preferences['download_content']
-    # download_excel_pref = global_preferences['download_excel']
-    # download_attachment_pref = global_preferences['download_attachment']
-    #
-    # try:
-    #     for letter in un_downloaded_letters:
-    #         letter.set_downloading()
-    #         utils.download(
-    #             letter,
-    #             download_pdf=download_pdf_pref and letter.has_pdf,
-    #             download_content=download_content_pref and letter.has_html,
-    #             download_excel=download_excel_pref and letter.has_excel,
-    #             download_attachment=download_attachment_pref and letter.has_attachment
-    #         )
-    #         letter.set_downloaded()
-    # except RequestException as e:
-    #     message = "به دلیل اختلال در اینترنت , دانلود گزارشات دانلود نشده با خطا مواجه شد."
-    #     error = str(e)
-    # except Exception as e:
-    #     message = "دانلود گزارشات دانلود نشده با خطای نامعلومی مواجه شد"
-    #     error = str(e)
-    # else:
-    #     message = "دانلود گزارشات دانلود نشده با موفقیت انجام شد"
-    #     error = ""
-    #
-    # Log.objects.create(
-    #     type=Log.Types.ERROR if error else Log.Types.SUCCESS,
-    #     message=message,
-    #     error=error
-    # )
-    #
-    # Letter.objects.filter(
-    #     status=Letter.Statuses.DOWNLOADING
-    # ).update(status=Letter.Statuses.RETRIEVED)
-    #
-    # Attachment.objects.filter(
-    #     status=Attachment.Statuses.DOWNLOADING
-    # ).update(status=Attachment.Statuses.RETRIEVED)
 
-    # if error:
-    #     Task.objects.get(
-    #         status=Task.Statuses.RUNNING,
-    #         type=Task.Types.RUNTIME,
-    #         task_type=Task.TaskTypes.DOWNLOAD,
-    #         celery_id=processor.DOWNLOAD_TASK_ID,
-    #     ).set_erred(error)
-    # else:
-    #     Task.objects.get(
-    #         status=Task.Statuses.RUNNING,
-    #         type=Task.Types.RUNTIME,
-    #         task_type=Task.TaskTypes.DOWNLOAD,
-    #         celery_id=processor.DOWNLOAD_TASK_ID
-    #     ).set_done()
+    un_downloaded_letters = Letter.objects.filter(status=Letter.Statuses.RETRIEVED)
 
-    # processor.DOWNLOAD_TASK_ID = None
+    global_preferences = global_preferences_registry.manager()
+    download_pdf_pref = global_preferences['download_pdf']
+    download_content_pref = global_preferences['download_content']
+    download_excel_pref = global_preferences['download_excel']
+    download_attachment_pref = global_preferences['download_attachment']
+
+    try:
+        for letter in un_downloaded_letters:
+            letter.set_downloading()
+            utils.download(
+                letter,
+                download_pdf=download_pdf_pref and letter.has_pdf,
+                download_content=download_content_pref and letter.has_html,
+                download_excel=download_excel_pref and letter.has_excel,
+                download_attachment=download_attachment_pref and letter.has_attachment
+            )
+            letter.set_downloaded()
+    except RequestException as e:
+        message = "به دلیل اختلال در اینترنت , دانلود گزارشات دانلود نشده با خطا مواجه شد"
+        error = str(e)
+    except Exception as e:
+        message = "دانلود گزارشات دانلود نشده با خطای نامعلومی مواجه شد"
+        error = str(e)
+    else:
+        message = "دانلود گزارشات دانلود نشده با موفقیت انجام شد"
+        error = ""
+
+    Log.objects.create(
+        type=Log.Types.ERROR if error else Log.Types.SUCCESS,
+        message=message,
+        error=error
+    )
+
+    Letter.objects.filter(
+        status=Letter.Statuses.DOWNLOADING
+    ).update(status=Letter.Statuses.RETRIEVED)
+
+    Attachment.objects.filter(
+        status=Attachment.Statuses.DOWNLOADING
+    ).update(status=Attachment.Statuses.RETRIEVED)
+
+    if error:
+        Task.objects.get(
+            status=Task.Statuses.RUNNING,
+            type=Task.Types.RUNTIME,
+            task_type=Task.TaskTypes.DOWNLOAD,
+            celery_id=processor.DOWNLOAD_TASK_ID,
+        ).set_erred(error)
+    else:
+        Task.objects.get(
+            status=Task.Statuses.RUNNING,
+            type=Task.Types.RUNTIME,
+            task_type=Task.TaskTypes.DOWNLOAD,
+            celery_id=processor.DOWNLOAD_TASK_ID
+        ).set_done()
+
+    processor.DOWNLOAD_TASK_ID = None
 
 
 # TODO Test
