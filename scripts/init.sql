b89420c16e7c469a7c0e431402e0dd618906e5b9
amin <mehdibs313@gmail.com>
2022-02-14 11:36:54 +0330

    Bugfix SQL Extraction [0] [Optimize Financial Statements Table Column Size]

--- scripts/init.sql
+++ scripts/init.sql
@@ -13,16 +13,16 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='$(SQL_TABLE)' and xtype='U')
 BEGIN
     CREATE TABLE $(SQL_TABLE) (
         Id INT PRIMARY KEY IDENTITY (1, 1),
-        col1 NVARCHAR(MAX),
-        col2 NVARCHAR(MAX),
-        col3 NVARCHAR(MAX),
-        col4 NVARCHAR(MAX),
-        col5 NVARCHAR(MAX),
-        col6 NVARCHAR(MAX),
-        col7 NVARCHAR(MAX),
-        col8 NVARCHAR(MAX),
-        col9 NVARCHAR(MAX),
-        col10 NVARCHAR(MAX),
-        col11 NVARCHAR(MAX),
+        col1 NVARCHAR(500),
+        col2 NVARCHAR(500),
+        col3 NVARCHAR(500),
+        col4 NVARCHAR(500),
+        col5 NVARCHAR(500),
+        col6 NVARCHAR(500),
+        col7 NVARCHAR(500),
+        col8 NVARCHAR(500),
+        col9 NVARCHAR(500),
+        col10 NVARCHAR(500),
+        col11 NVARCHAR(500),
     )
 END
