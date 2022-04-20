9056b3542ecc823b8f2ec939f5e9da701e1b2225
amin <mehdibs313@gmail.com>
2022-04-21 02:51:07 +0430

    SQL MVP [0] [Prepare]

--- codal/backends.py
+++ codal/backends.py
@@ -1,10 +1,12 @@
-import pyodbc
 import os
+import pyodbc
+import logging
 from bs4 import BeautifulSoup
 
 from codal.models import Report
-from codal.exceptions import ReportExtractorException
 from codal import utils
+
+logger = logging.getLogger(__name__)
 
 
 class BaseStoreBackend:
@@ -51,11 +53,70 @@ class ReportExtractorBackend:
         }
 
     def extract(self, report: Report):
-        defaults = self.ReportDefaults.get(report.type)
-        if not defaults:
-            raise ReportExtractorException("Report type undefined.")
+        # defaults = self.ReportDefaults.get(report.type)
+        # if not defaults:
+        #     raise ReportExtractorException("Report type undefined.")
+        #
+        # defaults['extraction_method'](report)
 
-        defaults['extraction_method'](report)
+        ### For MVP
+        with open(report.path, 'rt', encoding='utf-8') as f:
+            soup = BeautifulSoup(f, 'html.parser')
+
+            period_end_date = soup.select('#ctl00_lblPeriodEndToDate')[0].text if soup.select(
+                '#ctl00_lblPeriodEndToDate') else ''
+            report_name = soup.select('#ddlTable option[selected]')[0].text if soup.select(
+                '#ddlTable option[selected]') else ''
+            # report_name = utils.convert_report_type_name(report_name)
+            is_audited = soup.select('#ctl00_lblIsAudited')[0].text if soup.select('#ctl00_lblIsAudited') else ''
+            # symbol = soup.select('#ctl00_txbSymbol')[0].text  # Some Symbol Names Are Incorrect
+            symbol = report.letter.symbol
+            company = soup.select('#ctl00_txbCompanyName')[0].text if \
+                report.letter.company_name != soup.select('#ctl00_txbCompanyName')[0].text else 'اصلی'
+
+            fixed_columns = [period_end_date, report_name, is_audited, symbol, company]
+
+            max_header_size = 0
+            max_column_count = 35
+            tbl = "codal_tbl"
+
+            html_first_table = soup.select('table')[0]
+
+            for tr in html_first_table.select('thead tr'):
+                ths = [th.text for th in tr.select('th')] if len(tr.select('th')) >= max_header_size else [''] + [
+                    th.text for th in tr.select('th')] + ['', '']
+
+                row = ','.join([f"N'{data}'" for data in fixed_columns])
+                row += ','
+                row += ','.join([f"N'{th}'" for th in ths[:len(ths) - 1]])
+                row += ','
+                row += ','.join([f"N''" for i in range(1,
+                                                       max_column_count - len(ths) - len(
+                                                           fixed_columns) + 2)])
+
+                max_header_size = len(ths) if len(ths) > max_header_size else max_header_size
+
+                self._store.insert(
+                    table=tbl,
+                    values=row
+                )
+
+            for tr in html_first_table.select('tbody tr')[2:]:
+                tds = [td.text for td in tr.select('td')]
+
+                row = ','.join([f"N'{data}'" for data in fixed_columns])
+                row += ','
+                row += ','.join([f"N'{td}'" for td in tds[:len(tds) - 1]])
+                row += ','
+                row += ','.join([f"N''" for i in range(1,
+                                                       max_column_count - len(tds) - len(
+                                                           fixed_columns) + 2)])
+
+                self._store.insert(
+                    table=tbl,
+                    values=row
+                )
+        ### For MVP
 
     def _extract_financial_statements_report(self, report):
         with open(report.path, 'rt', encoding='utf-8') as f:
